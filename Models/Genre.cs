d43504e29cc5af60bb41521be054cd24b5781d1e
jackiecard <jacquecard@gmail.com>
2016-03-10 21:39:38 -0500

    first commit

---
+++ Models/Genre.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Music.Models
+{
+    public class Genre
+    {
+        public int GenreID { get; set; }
+        [Range(0, 20, ErrorMessage = "Name should have less than 20 characters")]
+        public string Name { get; set; }
+        public List<Album> Albums { get; set; }
+    }
+}
